4553da3c2f928f8a24c3afa2f1cc0b8f8dc90e7c
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-01 01:14:52 +0100

    predefined global functions in templates : using the "and" global with conditionals

---
+++ templates/predefined-global-functions-in-templates/using-and/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var templateFile *template.Template
+
+func init() {
+	templateFile = template.Must(template.ParseFiles("main.gohtml"))
+}
+
+type footballer struct {
+	Name     string
+	League   string
+	Country  string
+	Age      int
+	Position string
+}
+
+func main() {
+	bestFootballer := footballer{
+		Name:     "Cristiano Ronaldo",
+		League:   "Premier League",
+		Country:  "Portugal",
+		Age:      35,
+		Position: "Striker",
+	}
+
+	err := templateFile.ExecuteTemplate(os.Stdout, "main.gohtml", bestFootballer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
