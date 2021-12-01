2763c1c943cb35f1c3e8511fc6878aaa1b0c2803
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-01 15:32:32 +0100

    passing methods into templates

---
+++ templates/passing-composite-data-structures-into-templates/passing-methods/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
+
+var templateFile *template.Template
+
+func init() {
+	templateFile = template.Must(template.ParseFiles("main.gohtml"))
+}
+
+type footballer struct {
+	Name     string
+	League   string
+	Country  string
+	Age      int
+	Position string
+}
+
+func (f footballer) CapitalizeName() string {
+	return strings.ToUpper(f.Name)
+}
+func main() {
+	bestFootballer := footballer{
+		Name:     "Cristiano Ronaldo",
+		League:   "Premier League",
+		Country:  "Portugal",
+		Age:      35,
+		Position: "Striker",
+	}
+	award := struct {
+		AwardName     string
+		Footballer    footballer
+		EventLocation string
+	}{
+		AwardName:     "Ballon D'Or 2021",
+		Footballer:    bestFootballer,
+		EventLocation: "Paris, France",
+	}
+	err := templateFile.ExecuteTemplate(os.Stdout, "main.gohtml", award)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
