b544af10d84a344a43a25e5e5c95eb87ce7d475b
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-10 22:28:35 +0100

    using the http.Error and http error status code constants

--- server/serving-files/serve-content/main.go
+++ server/serving-files/serve-content/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -9,10 +10,10 @@ import (
 func main() {
 	http.HandleFunc("/", coin)
 	http.HandleFunc("/coin.jpg", coinPic)
-	http.ListenAndServe(":8080", nil)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
-func coin(w http.ResponseWriter, req *http.Request) {
+func coin(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	io.WriteString(w, `<img src="/coin.jpg">`)
 }
@@ -27,7 +28,7 @@ func coinPic(w http.ResponseWriter, req *http.Request) {
 
 	fi, err := f.Stat()
 	if err != nil {
-		http.Error(w, "file not found", 404)
+		http.Error(w, "file not found", http.StatusNotFound)
 		return
 	}
 
