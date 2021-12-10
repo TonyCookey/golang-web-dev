0be21f6e90e5776ee3e129c6aad82395b18614d7
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-10 16:56:50 +0100

    updated the hands-on exercise 1 for the http router sections to handle errors and remove unused variables

--- server/net-http/handson-exercises/01/main.go
+++ server/net-http/handson-exercises/01/main.go
@@ -9,20 +9,31 @@ import (
 func main() {
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/dog", indexHandler)
-	http.HandleFunc("/me", indexHandler)
+	http.HandleFunc("/dog", dogHandler)
+	http.HandleFunc("/me", meHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func indexHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "hello world")
+func indexHandler(res http.ResponseWriter, _ *http.Request) {
+	_, err := io.WriteString(res, "hello world")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
-func meHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "Anthony")
+func meHandler(res http.ResponseWriter, _ *http.Request) {
+	_, err := io.WriteString(res, "Anthony")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
-func dogHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "hello dog")
+func dogHandler(res http.ResponseWriter, _ *http.Request) {
+	_, err := io.WriteString(res, "hello dog")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
