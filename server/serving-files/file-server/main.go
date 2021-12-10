37f778281c1af50221b7f164586c093abb70b983
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-10 22:26:38 +0100

    using log.Fatalln() for the ListenAndServe Function call

--- server/serving-files/file-server/main.go
+++ server/serving-files/file-server/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/dog/", dog)
-	http.ListenAndServe(":8080", nil)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
-func dog(w http.ResponseWriter, req *http.Request) {
+func dog(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="/toby.jpg">`)
+	_, err := io.WriteString(w, `<img src="/toby.jpg">`)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
