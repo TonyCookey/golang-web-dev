885d8749fe83c161ccb4085336180002b0174eb0
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-15 13:59:51 +0100

    read and set cookies to the browser/client

---
+++ server/cookies/set-read-cookies-browser/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(w http.ResponseWriter, _ *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "tony cookey is awesome",
+		Path:  "/",
+	})
+
+}
+
+func read(w http.ResponseWriter, req *http.Request) {
+
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "YOUR COOKIE:", c)
+}
